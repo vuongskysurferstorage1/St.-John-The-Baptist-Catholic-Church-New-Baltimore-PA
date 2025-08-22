533f78303e46426801aa68fae9909572c681f362
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-22 01:39:18 -0400

    update

--- data.js
+++ data.js
@@ -593,7 +593,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
