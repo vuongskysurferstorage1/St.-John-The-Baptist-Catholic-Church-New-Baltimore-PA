c1cf515f9fd3193c7dc0c2e9ab24462a97977432
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-22 01:29:01 -0400

    360 Upload

---
+++ data.js
@@ -0,0 +1,599 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.039372110231198576,
+        "pitch": -0.2299296328270639,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02526087873378735,
+          "pitch": -1.2445019826160184,
+          "rotation": 0,
+          "target": "1-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -0.006307874362040167,
+          "pitch": -0.140884996530513,
+          "rotation": 0,
+          "target": "3-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 2.974803088295868,
+          "pitch": 0.17950495933500044,
+          "rotation": 0,
+          "target": "2-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.06395593877028816,
+        "pitch": 0.01264362590002932,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11411892034454851,
+          "pitch": 1.5685757525509647,
+          "rotation": 0,
+          "target": "0-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0.45641396079176744,
+          "pitch": -0.35255505419723754,
+          "rotation": 0,
+          "target": "5-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0.5498285860122429,
+          "pitch": 0.07143908086010953,
+          "rotation": 0,
+          "target": "4-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0.05140544279771575,
+          "pitch": 1.23444364599548,
+          "rotation": 0,
+          "target": "3-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 2.8760890151771967,
+          "pitch": 1.1086396836384953,
+          "rotation": 0,
+          "target": "2-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6111587967672065,
+          "pitch": -0.2076416242801944,
+          "rotation": 0,
+          "target": "3-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -1.8428421758695084,
+          "pitch": -0.03508213687293171,
+          "rotation": 0,
+          "target": "0-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -1.107984794758611,
+          "pitch": -0.36267330519617325,
+          "rotation": 0,
+          "target": "5-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -1.607413611172941,
+          "pitch": -1.0104277272649966,
+          "rotation": 0,
+          "target": "1-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -1.0333502917074284,
+          "pitch": -0.12535609067300513,
+          "rotation": 0,
+          "target": "4-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0018142232447502,
+          "pitch": 0.23682326708721035,
+          "rotation": 0,
+          "target": "2-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -2.9194640821097497,
+          "pitch": 0.5641247400689622,
+          "rotation": 0,
+          "target": "0-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 2.8468935816995415,
+          "pitch": -1.5423323391740702,
+          "rotation": 0,
+          "target": "1-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "7-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04438240167991658,
+          "pitch": -0.1082760503256921,
+          "rotation": 0,
+          "target": "1-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 2.5488943448536014,
+          "pitch": -0.6998431460213119,
+          "rotation": 0,
+          "target": "5-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -0.09959545332068487,
+          "pitch": 0.077181502129271,
+          "rotation": 0,
+          "target": "2-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11192004015616597,
+          "pitch": 0.502184500338636,
+          "rotation": 0,
+          "target": "4-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -2.9723503152594937,
+          "pitch": -1.298016157026023,
+          "rotation": 0,
+          "target": "6-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06960068108156925,
+          "pitch": 1.194982316186426,
+          "rotation": 0,
+          "target": "5-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -0.09989150106938283,
+          "pitch": 0.5418490580812971,
+          "rotation": 0,
+          "target": "4-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.099405675197029,
+          "pitch": 0.39469555932965683,
+          "rotation": 0,
+          "target": "3-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -0.00887972375861068,
+          "pitch": 0.0951747731921806,
+          "rotation": 0,
+          "target": "8-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1328730113721655,
+          "pitch": 0.061657120565318024,
+          "rotation": 0,
+          "target": "7-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": -0.008882164198688258,
+          "pitch": -0.031095362266007953,
+          "rotation": 0,
+          "target": "10-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.001451240233127038,
+          "pitch": 0.06492056823446468,
+          "rotation": 0,
+          "target": "8-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0.05356099121576818,
+          "pitch": -1.274828369368631,
+          "rotation": 0,
+          "target": "10-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-st-john-the-baptist-catholic-church-new-baltimore-pa",
+      "name": "St. John The Baptist Catholic Church New Baltimore, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16388866628088294,
+          "pitch": 1.318727980033854,
+          "rotation": 0,
+          "target": "9-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        },
+        {
+          "yaw": 0.0022413951855426717,
+          "pitch": 0.23631467467978773,
+          "rotation": 0,
+          "target": "8-st-john-the-baptist-catholic-church-new-baltimore-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
